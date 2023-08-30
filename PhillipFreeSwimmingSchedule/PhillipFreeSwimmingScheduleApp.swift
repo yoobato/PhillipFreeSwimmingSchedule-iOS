b99e5fc914be5687a1ea4c120aadb8855b8d172f
yoobato <yoobato@gmail.com>
2023-08-30 23:11:41 +0900

    Skeleton project

---
+++ PhillipFreeSwimmingSchedule/PhillipFreeSwimmingScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhillipFreeSwimmingScheduleApp.swift
+//  PhillipFreeSwimmingSchedule
+//
+//  Created by Daeyeol Ryu on 2023/08/30.
+//
+
+import SwiftUI
+
+@main
+struct PhillipFreeSwimmingScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
